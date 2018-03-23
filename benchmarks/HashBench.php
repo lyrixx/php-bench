61c2c68e8755d0e41d04bdf3ef72f7a4c78450b5
Grégoire Pineau <lyrixx@lyrixx.info>
2018-03-23 14:32:19 +0100

    Hash bench

---
+++ benchmarks/HashBench.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Processor;
+
+/**
+ * @Revs(1000000)
+ * @Iterations(5)
+ */
+class HashBench
+{
+    public function benchSha1()
+    {
+        sha1('foo');
+    }
+
+    public function benchHashSha1()
+    {
+        hash('sha1', 'foo');
+    }
+
+    public function benchMd5()
+    {
+        md5('foo');
+    }
+
+    public function benchHashMd5()
+    {
+        hash('md5', 'foo');
+    }
+}
