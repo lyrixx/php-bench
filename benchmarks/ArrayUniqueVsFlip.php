e98f8e9dd56a311565a51b3462d654c520b627e2
Grégoire Pineau <lyrixx@lyrixx.info>
2020-03-27 13:36:19 +0100

    Bench array_unique vs array_bench
    ```
    +------------------+----------+-------+
    | subject          | mean     | diff  |
    +------------------+----------+-------+
    | benchArrayUnique | 43.619μs | 3.26x |
    | benchArrayFlip   | 13.399μs | 1.00x |
    +------------------+----------+-------+
    ```

---
+++ benchmarks/ArrayUniqueVsFlip.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @Revs(1000)
+ * @Iterations(1)
+ * @BeforeMethods({"init"})
+ */
+class ArrayUniqueVsFlip
+{
+    private $array;
+
+    public function init()
+    {
+        $this->array = [];
+        for ($i = 0; $i < 1000; $i++) {
+            $this->array[] = random_int(0, 1000);
+        }
+    }
+
+    public function benchArrayUnique()
+    {
+        array_unique($this->array);
+    }
+
+    public function benchArrayFlip()
+    {
+        array_keys(array_flip($this->array));
+    }
+}
