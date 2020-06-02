42c3bb3af9e80fa492526e2dd786de40e6198465
Grégoire Pineau <lyrixx@lyrixx.info>
2020-06-02 11:31:09 +0200

    Bench also with 5 args
    +----------------------------------+---------+-------+
    | subject                          | mean    | diff  |
    +----------------------------------+---------+-------+
    | benchArrayMerge                  | 0.165μs | 1.00x |
    | benchArrayDestructuring          | 0.194μs | 1.18x |
    | benchArrayMergeWith5Args         | 0.285μs | 1.73x |
    | benchArrayDestructuringWith5Args | 0.378μs | 2.29x |
    +----------------------------------+---------+-------+

--- benchmarks/ArrayMergeVsArrayDestructuringBench.php
+++ benchmarks/ArrayMergeVsArrayDestructuringBench.php
@@ -9,11 +9,17 @@ class ArrayMergeVsArrayDestructuringBench
 {
     private $array1;
     private $array2;
+    private $array3;
+    private $array4;
+    private $array5;
 
     public function init()
     {
         $this->array1 = range('a', 'd');
         $this->array2 = range(0, 10);
+        $this->array3 = range('e', 'p');
+        $this->array4 = range('q', "z");
+        $this->array5 = range(11, 20);
     }
 
     public function benchArrayMerge()
@@ -24,5 +30,15 @@ class ArrayMergeVsArrayDestructuringBench
     public function benchArrayDestructuring()
     {
         [...$this->array1, ...$this->array2];
+    }
+
+    public function benchArrayMergeWith5Args()
+    {
+        array_merge($this->array1, $this->array2, $this->array3, $this->array4, $this->array5);
+    }
+
+    public function benchArrayDestructuringWith5Args()
+    {
+        [...$this->array1, ...$this->array2, $this->array3, ...$this->array4, ...$this->array5];
     }
 }
