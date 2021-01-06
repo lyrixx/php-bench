b019fefd5d48bb330f9b3def7ab3dd44d413e04c
Grégoire Pineau <lyrixx@lyrixx.info>
2021-01-06 17:57:13 +0100

    Added bench for cast vs === vs instanceof
    ```
    +----------------------------+---------+-------+
    | subject                    | mean    | diff  |
    +----------------------------+---------+-------+
    | benchWithNullAndCast       | 0.083μs | 1.00x |
    | benchWithNullAndSame       | 0.086μs | 1.04x |
    | benchWithNullAndInstanceOf | 0.083μs | 1.00x |
    | benchWithObjAndCast        | 0.083μs | 1.01x |
    | benchWithObjAndSame        | 0.085μs | 1.03x |
    | benchWithObjAndInstanceOf  | 0.086μs | 1.04x |
    +----------------------------+---------+-------+
    ```

---
+++ benchmarks/Cast.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @Revs(100)
+ * @Iterations(100)
+ * @BeforeMethods({"init"})
+ */
+class Cast
+{
+    private $nullData;
+    private $objData;
+
+    public function init()
+    {
+        $this->nullData = null;
+        $this->objData = new DateTime();
+    }
+
+    public function benchWithNullAndCast()
+    {
+        if ($this->nullData) {
+
+        }
+    }
+
+    public function benchWithNullAndSame()
+    {
+        if (null !== $this->nullData) {
+
+        }
+    }
+
+    public function benchWithNullAndInstanceOf()
+    {
+        if ($this->nullData instanceof \DateTime) {
+        }
+    }
+
+    public function benchWithObjAndCast()
+    {
+        if ($this->objData) {
+
+        }
+    }
+
+    public function benchWithObjAndSame()
+    {
+        if (null !== $this->objData) {
+
+        }
+    }
+
+    public function benchWithObjAndInstanceOf()
+    {
+        if ($this->objData instanceof \DateTime) {
+        }
+    }
+}
